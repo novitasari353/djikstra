02f7b7804ebcc93bd9a89c6109283f7e3b9d3813
Novitasari <59106885+novitasari353@users.noreply.github.com>
2019-12-23 10:20:32 +0700

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -34,5 +34,15 @@ class Graph:
         for edge in edges:
             if [edge.start, edge.end] in node_pairs:
                 self.edges.remove(edge)
+   
+    def add_edge(self, n1, n2, cost=1, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        for edge in self.edges:
+            if [edge.start, edge.end] in node_pairs:
+                return ValueError('Edge {} {} already exists'.format(n1, n2))
+
+        self.edges.append(Edge(start=n1, end=n2, cost=cost))
+        if both_ends:
+            self.edges.append(Edge(start=n2, end=n1, cost=cost))
 
 
