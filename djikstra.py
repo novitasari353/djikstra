86125b85fe3bebaae9da575975893c768e57de71
Novitasari <59106885+novitasari353@users.noreply.github.com>
2019-12-23 10:18:19 +0700

    Add files via upload

--- djikstra.py
+++ djikstra.py
@@ -1,5 +1,5 @@
-from collections import deque
 from collections import namedtuple
+from collections import deque
 
 inf = float('inf')
 Edge = namedtuple('Edge', 'start, end, cost')
@@ -8,10 +8,8 @@ Edge = namedtuple('Edge', 'start, end, cost')
 def make_edge(start, end, cost=1):
   return Edge(start, end, cost)
 
-
 class Graph:
     def __init__(self, edges):
-
         wrong_edges = [i for i in edges if len(i) not in [2, 3]]
         if wrong_edges:
             raise ValueError('edges data is wrong: {}'.format(wrong_edges))
@@ -19,5 +17,22 @@ class Graph:
         self.edges = [make_edge(*edge) for edge in edges]
 
 
+    def vertices(self):
+        return set( sum(([edge.start, edge.end] for edge in self.edges), [] ))
+
+    def get_node_pairs(self, n1, n2, both_ends = True):
+        if both_ends:
+            node_pairs = [[n1, n2],
+                          [n2, n1]]
+        else:
+            node_pairs = [[n1, n2]]
+        return node_pairs
+
+    def remove_edge(self, n1, n2, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        edges = self.edges[:]
+        for edge in edges:
+            if [edge.start, edge.end] in node_pairs:
+                self.edges.remove(edge)
 
 
